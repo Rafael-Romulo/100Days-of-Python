ee1f1936ea93da018f49bf134ca23c148a1c7a49
Rafael-Romulo <69024137+Rafael-Romulo@users.noreply.github.com>
2021-11-29 10:55:03 -0300

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -2,4 +2,5 @@ price = float(input("\tWelcome to the tip calculator\nWhat was the total bill? "
 tip_percentage = float(input("What percentage tip would you like to give? 10, 12 or 15? "))
 people_in_the_table = int(input("How many people to split the bill? "))
 individual_price = (price*(tip_percentage/100 + 1))/people_in_the_table
-print(str(individual_price))
+final_amount = round(individual_price, 2)
+print(f"Each person should pay: ${final_amount}")
