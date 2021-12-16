48e38baff31327c51b84bdadb21ada676ea944d1
Rafael-Romulo <69024137+Rafael-Romulo@users.noreply.github.com>
2021-12-16 15:30:44 -0300

    Add files via upload

---
+++ Quiz Project/data.py
@@ -0,0 +1,23 @@
+question_data = [
+    {"text": "The programming language Python is based off a modified version of JavaScript.",
+     "answer": "False"},
+    {
+     "text": "Pointers were not used in the original C programming language; they were added later on in C++.",
+     "answer": "False"},
+
+    {"text": "RAM stands for Random Access Memory.", "answer": "True"},
+
+    {"text": "Ada Lovelace is often considered the first computer programmer.", "answer": "True"},
+
+    {"text": "HTML stands for Hypertext Markup Language.", "answer": "True"},
+
+    {"text": "In most programming languages, the operator ++ is equivalent to the statement {variable = variable +1}.", "answer": "True"},
+
+    {"text": "Time on Computers is measured via the EPOX System.", "answer": "False"},
+
+    {"text": "The Windows 7 operating system has six main editions.", "answer": "True"},
+
+    {"text": "Linux was first created as an alternative to Windows XP.", "answer": "False"},
+
+    {"text": "The Python programming language gets its name from the British comedy group Monty Python","answer": "True"}
+]
