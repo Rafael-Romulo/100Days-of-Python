e70e4ae8d3e3dcadeb904b89df7602a56db25a2b
Rafael-Romulo <69024137+Rafael-Romulo@users.noreply.github.com>
2021-12-20 16:44:00 -0300

    Add files via upload

---
+++ Geometric Shapes.py
@@ -0,0 +1,31 @@
+from turtle import Screen, Turtle
+from random import random
+
+
+screen = Screen()
+Lentin = Turtle()
+Lentin.speed("fastest")
+
+answer = int(screen.textinput(title = "Geometry", prompt= "Type any number above 3")) + 1
+
+def change_color(turtle):
+    r = random()
+    g = random()
+    b = random()
+    random_color = (r, g, b)
+    turtle.color(random_color)
+
+
+def draw(sides):
+    for _ in range(sides):
+        Lentin.forward(80)
+        Lentin.right(360- 360/sides)
+        Lentin.end_fill()
+
+
+for i in range(3, answer):
+    change_color(Lentin)
+    draw(i)
+
+
+screen.exitonclick()
